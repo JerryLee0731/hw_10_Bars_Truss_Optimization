57d59535b4d6c5875d1da5f020aeea481ee04b80
JerryLee0731 <f5air0731@gmail.com>
2023-08-11 22:33:59 +0800

    step5 finished

--- sol_TenBarTruss.py
+++ sol_TenBarTruss.py
@@ -25,10 +25,10 @@ class Element:
         c = self.cos
         s = self.sin
         k = numpy.array([[c**2, c*s, -c**2, -c*s], [c*s, s**2, -c*s, -s**2], [-c**2, -c*s, c**2, c*s], [-c*s, -s**2, c*s, s**2]])
-        self.k = k
+        self.K_each = k*self.E*self.A/self.L
 
-r1 = 10
-r2 = 15
+r1 = 0.10
+r2 = 0.05
 
 element1 = Element(node3, node5, r1)
 element2 = Element(node1, node3, r1)
@@ -43,20 +43,49 @@ element10 = Element(node1, node4, r2)
 
 elements = [element1, element2, element3, element4, element5, element6, element7, element8, element9, element10]
 
-for i in range(10):
-    print(elements[i].k)
-
 K = numpy.zeros((12,12))
 for i in range(10):
     a = elements[i].nodei.node_id*2-1
     b = elements[i].nodej.node_id*2-1
     
-    k_temp  = elements[i].k
+    k_temp  = elements[i].K_each
     K[a-1:a+1, a-1:a+1] = K[a-1:a+1, a-1:a+1]+ k_temp[0:2, 0:2]
     K[a-1:a+1, b-1:b+1] = K[a-1:a+1, b-1:b+1]+ k_temp[0:2, 2:4]
     K[b-1:b+1, a-1:a+1] = K[b-1:b+1, a-1:a+1]+ k_temp[2:4, 0:2]
     K[b-1:b+1, b-1:b+1] = K[b-1:b+1, b-1:b+1]+ k_temp[2:4, 2:4]
-    print("K now:")
-    print(K)
 
-print(K)
+print("K=", K)
+
+f = numpy.zeros(12)
+f[3] = 10**7
+f[7] = 10**7
+
+q = numpy.zeros(12)
+
+K_reduced = K[:8, :8]
+f_reduced = f[:8]
+
+K_reduced_inv = numpy.linalg.inv(K_reduced)
+q_reduced = numpy.dot(K_reduced_inv, f_reduced)
+print(q_reduced)
+q[:8] = q_reduced
+print("q= ", q)
+
+sigma = numpy.zeros(12)
+for i in range(10):
+    a = elements[i].nodei.node_id*2-1
+    b = elements[i].nodej.node_id*2-1
+    
+    A = [-elements[i].cos, -elements[i].sin, elements[i].cos, elements[i].sin]
+    q_element = [q[a-1], q[a], q[b-1], q[b]]
+    sigma[a-1] = elements[i].E/elements[i].L*numpy.dot(A, q_element)
+
+print("sigma= ", sigma)
+K_reaction = K[8:12, :]
+R = numpy.dot(K_reaction, q.T)
+
+print("R=", R)
+
+
+
+
